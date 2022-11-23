8930bb3dd0a5ced5a6c2cb40ba228ddc42fe6011
Vitaly <72764133+vitlyc@users.noreply.github.com>
2022-11-23 22:36:50 +0300

    delayu

--- models/Todo.js
+++ models/Todo.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose")
 
 const TodoSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: [true, "добавьте текст"],
-    trim: true,
-    maxlength: [300, "не более 300 символов"],
-    minlength: [1, "не менее 1 символа"],
-  },
+    text: {
+        type: String,
+        required: [true, "добавьте текст"],
+        trim: true,
+        maxlength: [300, "не более 300 символов"],
+        minlength: [2, "не менее 2 символов"],
+    },
 })
+module.exports = mongoose.model("Todo", TodoSchema)
